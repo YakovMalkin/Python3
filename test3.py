1cd02a969033def3b8f8c36c3b82b9f24a00ef60
YakovMalkin <yakov.malkin@gmail.com>
2017-03-25 17:22:28 +0300

    New_files_python

--- test3.py
+++ test3.py
@@ -71,27 +71,19 @@ Nine  = [" ****",
 
 Big_data = [Zero, One, Two, Three, Four, Five, Six, Seven, Eight, Nine]
 
-while True:
-    try:
-        line = input("Input number: ")
-        if line:
-            row = 0
-            while row < 7:
-                col = 0
-                tmp_line = ""
-                while col < len(line):
-                        digits = int(line[col])
-                        element = Big_data[digits]
-
-                        tmp_line += element[row].replace('*', line[col])
-
-                        col += 1
-                print(tmp_line)
-                row += 1;
-            else:
-                break
-        else:
-            break
-    except ValueError as err:
-        print("Ошибка - ", err)
-        break
+try:
+    digits = input("Input number: ")
+    if digits:
+        heigth = len(Zero)
+        row = 0
+        while row < heigth:
+            line = ''
+            for number in digits:
+                line += Big_data[int(number)][row].replace('*', number)
+                line += '  '
+            print(line)
+            row += 1
+except IndexError:
+    print("usage: bigdigits.py <number>")
+except ValueError as err:
+    print(err, "in", digits)
